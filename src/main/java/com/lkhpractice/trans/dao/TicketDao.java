94e43ddf7b6c9ed02bdce9bf1e660c1599942f24
ici <ici@DESKTOP-CL6HLKL>
2023-04-26 15:50:48 +0900

    백업

--- src/main/java/com/lkhpractice/trans/dao/TicketDao.java
+++ src/main/java/com/lkhpractice/trans/dao/TicketDao.java
@@ -7,6 +7,9 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.lkhpractice.trans.dto.CardDto;
 
@@ -14,39 +17,87 @@ public class TicketDao {
 	
 	JdbcTemplate template;
 	
+	TransactionTemplate transactionTemplate;
+	
 	public void setTemplate(JdbcTemplate template) {
 		this.template = template;
 	}
 	
-	public void buyTicket(final CardDto dto) {
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+		this.transactionTemplate = transactionTemplate;
+	}
+	
+	
+//	public void buyTicket(final CardDto dto) {	//트랜잭션 미적용
+//		
+//		this.template.update(new PreparedStatementCreator() {
+//			
+//			@Override
+//			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+//				
+//				String sql = "INSERT INTO card (consumerid, amount) VALUES (?,?)";
+//				PreparedStatement pstmt = conn.prepareStatement(sql);
+//				pstmt.setString(1, dto.getConsumerid());
+//				pstmt.setString(2, dto.getAmount());
+//				
+//				return pstmt;
+//			}
+//		});
+//		
+//		this.template.update(new PreparedStatementCreator() {
+//			
+//			@Override
+//			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+//				
+//				String sql = "INSERT INTO ticket (consumerid, countnum) VALUES (?,?)";
+//				PreparedStatement pstmt = conn.prepareStatement(sql);
+//				pstmt.setString(1, dto.getConsumerid());
+//				pstmt.setString(2, dto.getAmount());
+//				
+//				return pstmt;
+//			}
+//		});
+//	}
+
+
+	public void buyTicket(final CardDto dto) {	//트랜잭션 적용
 		
-		this.template.update(new PreparedStatementCreator() {
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 			
 			@Override
-			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
 				
-				String sql = "INSERT INTO card (consumerid, amount) VALUES (?,?)";
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, dto.getConsumerid());
-				pstmt.setString(2, dto.getAmount());
+				template.update(new PreparedStatementCreator() {
+					
+					@Override
+					public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+						
+						String sql = "INSERT INTO ticket (consumerid, countnum) VALUES (?,?)";
+						PreparedStatement pstmt = conn.prepareStatement(sql);
+						pstmt.setString(1, dto.getConsumerid());
+						pstmt.setString(2, dto.getAmount());
+						
+						return pstmt;
+					}
+				});
 				
-				return pstmt;
+				template.update(new PreparedStatementCreator() {
+					
+					@Override
+					public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+						
+						String sql = "INSERT INTO card (consumerid, amount) VALUES (?,?)";
+						PreparedStatement pstmt = conn.prepareStatement(sql);
+						pstmt.setString(1, dto.getConsumerid());
+						pstmt.setString(2, dto.getAmount());
+						
+						return pstmt;
+					}
+				});
+
 			}
 		});
-		
-		this.template.update(new PreparedStatementCreator() {
-			
-			@Override
-			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-				
-				String sql = "INSERT INTO ticket (consumerid, countnum) VALUES (?,?)";
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, dto.getConsumerid());
-				pstmt.setString(2, dto.getAmount());
-				
-				return pstmt;
-			}
-		});
+
 	}
 	
 }
